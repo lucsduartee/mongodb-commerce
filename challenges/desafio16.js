9bf368c5dd8b367f44b71dd2601551ca4b882264
Lucas Duarte <lucs.rkt@gmail.com>
2022-06-28 23:15:49 -0300

    feat: challenge 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany({
+  nome: "Big Mac",
+}, {
+  $currentDate: {
+    ultimaModificacao: {
+      $type: "timestamp",
+    },
+  },
+});
+
+db.produtos.find({
+  ultimaModificacao: {
+    $exists: true,
+  },
+}, {
+  nome: true,
+  _id: false,
+});
