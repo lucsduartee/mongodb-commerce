76a2d9eaf82abb95cca4a7985842af0197c52325
Lucas Duarte <lucs.rkt@gmail.com>
2022-06-29 00:33:46 -0300

    feat: challenges 24~26

---
+++ challenges/desafio26.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio",
+      $and: [
+        { percentual: { $gt: 20 } },
+        { percentual: { $lt: 40 } },
+      ],
+    },
+  },
+}, {
+  $push: {
+    tags: "contém sódio",
+  },
+});
+
+db.produtos.find({}, {
+  nome: true,
+  tags: true,
+  _id: false,
+});
