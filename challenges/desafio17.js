9e89a8195470bcb0e0c58af50229191bd6512139
Lucas Duarte <lucs.rkt@gmail.com>
2022-06-28 23:26:14 -0300

    feat: challenge 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,9 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds", totalProdutos: db.produtos.find({}).count(),
+});
+
+db.resumoProdutos.find({}, {
+  franquia: true,
+  totalProdutos: true,
+  _id: false,
+});
